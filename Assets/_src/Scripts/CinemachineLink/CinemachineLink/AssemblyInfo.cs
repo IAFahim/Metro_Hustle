59d08e3a1ead2e36e82242a91e0608a2ad201c87
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2025-05-12 13:13:30 +0600

    Working on camera

---
+++ Assets/_src/Scripts/CinemachineLink/CinemachineLink/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+// <copyright file="AssemblyInfo.cs" company="DefaultCompany">
+// Copyright (c) DefaultCompany. All rights reserved.
+// </copyright>
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("CinemachineLink.Debug")]
+[assembly: InternalsVisibleTo("CinemachineLink.Editor")]
+[assembly: InternalsVisibleTo("CinemachineLink.Tests")]
