a19effc7b6c5cd5a1aed31eb91c070ab31c4122b
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2025-05-13 05:09:10 +0600

    wip camera focsu

---
+++ Assets/_src/Scripts/Focuses/Focuses.Data/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+// <copyright file="AssemblyInfo.cs" company="DefaultCompany">
+// Copyright (c) DefaultCompany. All rights reserved.
+// </copyright>
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Focuses")]
+[assembly: InternalsVisibleTo("Focuses.Authoring")]
+[assembly: InternalsVisibleTo("Focuses.Debug")]
+[assembly: InternalsVisibleTo("Focuses.Editor")]
+[assembly: InternalsVisibleTo("Focuses.Tests")]
---
+++ Assets/_src/Scripts/Focuses/Focuses.Data/FocusManagerCurrentInfoComponent.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+namespace _src.Scripts.Focuses.Focuses.Data
+{
+    public struct FocusManagerCurrentInfoComponent : IComponentData
+    {
+        public float3 Position;
+        public quaternion Rotation;
+    }
+}
