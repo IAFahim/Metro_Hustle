4572a46a978aed1aff70f36d6b48850488b1cb6c
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2025-05-13 10:51:56 +0600

    Box movement (#3)
    * Testing error
    
    * raod gap fixed

--- Assets/SplineMesh/Runtime/Core/SplineMeshUtils.cs
+++ Assets/SplineMesh/Runtime/Core/SplineMeshUtils.cs
@@ -6,39 +6,31 @@ namespace SplineMesh.SplineMesh.Runtime.Core
     {
 		public static Vector2 MakeUVs(Vector2 uv, float point, VectorAxis uvAxis, float uvResolutions)
 		{
-			switch (uvAxis)
+			return uvAxis switch
 			{
-				case VectorAxis.X:
-					return new Vector2(point * uvResolutions, uv.y);
-				default:
-					return new Vector2(uv.x, point * uvResolutions);
-			}
+				VectorAxis.X => new Vector2(point * uvResolutions, uv.y),
+				_ => new Vector2(uv.x, point * uvResolutions)
+			};
 		}
 
 		public static Vector3 GetRequiredOffset(Vector3 vector, VectorAxis axis)
 		{
-			switch (axis)
+			return axis switch
 			{
-				case VectorAxis.X:
-					return new Vector3(vector.y, vector.z, 0f);
-
-				default:
-				case VectorAxis.Y:
-					return new Vector3(vector.x, vector.z, 0f);
-			}
+				VectorAxis.X => new Vector3(vector.y, vector.z, 0f),
+				VectorAxis.Y => new Vector3(vector.x, vector.z, 0f),
+				_ => new Vector3(vector.x, vector.z, 0f)
+			};
 		}
 
 		public static float GetRequiredAxis(Vector3 vector, VectorAxis axis)
 		{
-			switch (axis)
+			return axis switch
 			{
-				case VectorAxis.X:
-					return vector.x;
-
-				default:
-				case VectorAxis.Y:
-					return vector.y;
-			}
+				VectorAxis.X => vector.x,
+				VectorAxis.Y => vector.y,
+				_ => vector.y
+			};
 		}
     }
 }
