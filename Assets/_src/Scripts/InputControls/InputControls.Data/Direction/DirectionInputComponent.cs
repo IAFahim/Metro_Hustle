36342b25f6701650ad7cf748c1acb0584128fd26
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2025-05-20 12:35:59 +0600

    Left right movement (#6)
    * Init
    
    * Working on Input
    
    * All4Direction
    
    * Update workspace.xml
    
    * Organizing Gen Code
    
    * mesh offset works
    
    * Creating job version
    
    * almost job
    
    * Wip:Input live
    
    * Input works
    
    * Update workspace.xml
    
    * Update Aline
    
    * Spline Movement and collision
    
    * Refactoring color
    
    * Forgot to save scene
    
    * Update workspace.xml
    
    * LocalToWorld instead of LocalTransfrom
    
    * Collision works
    
    * Collision Sphere works
    
    * Stuck on ordering
    
    * Wip
    
    * Intransic pending
    
    * Going to intrastic system
    
    * Removed stats and reaction
    
    * Light refactor
    
    * Working on notify
    
    * Tip Works
    
    * Update workspace.xml
    
    * Trigger works
    
    * main connect
    
    * after close

--- Assets/_src/Scripts/InputControls/InputControls.Data/Direction/DirectionInputComponent.cs
+++ Assets/_src/Scripts/InputControls/InputControls.Data/Direction/DirectionInputComponent.cs
@@ -1,7 +1,9 @@
-﻿using Unity.Entities;
+﻿using BovineLabs.Core;
+using Unity.Entities;
 
 namespace _src.Scripts.InputControls.InputControls.Data.Direction
 {
+    [ChangeFilterTracking]
     public struct DirectionInputComponent : IComponentData
     {
         public DirectionFlag DirectionFlag;
